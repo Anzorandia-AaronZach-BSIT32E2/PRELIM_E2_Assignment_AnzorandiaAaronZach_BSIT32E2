d61e6d0bd9515d206f0e0da2a19c97fccec2aeb3
Anzorandia_Aaron Zach_BSIT32E2 <zanzorandia@gmail.com>
2024-03-02 02:34:05 +0800

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+using PRELIM_E3_Assignment_AnzorandiaAaronZach_BSIT32E2.Models;
+using System.Diagnostics;
+
+namespace PRELIM_E3_Assignment_AnzorandiaAaronZach_BSIT32E2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult School()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PRELIM_E3_Assignment_AnzorandiaAaronZach_BSIT32E2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
